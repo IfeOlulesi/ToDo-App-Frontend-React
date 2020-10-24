c135485d8bf3a2302f1c2e4e37a06962d16879a7
Tekena Solomon <tekena157@gmail.com>
2020-10-24 13:41:18 +0100

    initialized app

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./styles/App.css";
+import { Container } from "./styles/Styles";
+
+const App = () => {
+  return <Container className="App">todo app</Container>;
+};
+
+export default App;
